05af5fa604bde1c4172320ad19db38b00d3b36e4
kairzhan8 <kairzhankural@gmail.com>
2020-09-15 17:08:03 +0600

    Initial Commit

---
+++ TaskList with Realm/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TaskList with Realm
+//
+//  Created by Kairzhan Kural on 9/15/20.
+//  Copyright © 2020 Kairzhan Kural. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
